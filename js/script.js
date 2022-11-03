cabb666db918c9e10f6fdc5074559ef7ceea399f
MarBozzz <mbozzuto@libero.it>
2022-11-03 16:20:15 +0100

    [added] selected pic + thumbs

--- js/script.js
+++ js/script.js
@@ -1,30 +1,42 @@
+/*Descrizione:
+Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
+- al click su una thumb, visualizzare in grande l’immagine corrispondente
+- aggiungere la classe active alla thumb attivaBonus:
+1- applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+2- quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
+*/
 
-//creo l'array
-const carouselData = [
-  {
-    titolo : 'Argentina',
-    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque exercitatione',
-    image :'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg'
-  },
-  {
-    titolo : 'Chile',
-    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque',
-    image : 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c'
-  },
-  {
-    titolo : 'Colombia',
-    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    image : 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop'
-  },
-  {
-    titolo : 'Peru',
-    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque exercitationem quaerat',
-    image : 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg'
-  },
-  {
-    titolo : 'Sweden',
-    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    image : 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg'
+const {createApp} = Vue;
 
+createApp ({
+  data(){
+    return {
+      carouselData : {
+        images : [
+            'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+            'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+            'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+            'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+            'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg'
+        ],
+        titles : [
+          'Argentina',
+          'Chile',
+          'Colombia',
+          'Peru',
+          'Sweden',
+        ],
+        descriptions : [
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque exercitatione',
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque',
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque exercitationem quaerat','Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        ]
+      },
+      activeImage : 0
+    }
+  },
+  methods: {
+    //functions
   }
-];
+}).mount('#app');
