b515473ecfb413192d6f932e83278bf45315f07d
MarBozzz <mbozzuto@libero.it>
2022-11-03 14:29:57 +0100

    [init]

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+//creo l'array
+const carouselData = [
+  {
+    titolo : 'Argentina',
+    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque exercitatione',
+    image :'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg'
+  },
+  {
+    titolo : 'Chile',
+    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque',
+    image : 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c'
+  },
+  {
+    titolo : 'Colombia',
+    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    image : 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop'
+  },
+  {
+    titolo : 'Peru',
+    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque exercitationem quaerat',
+    image : 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg'
+  },
+  {
+    titolo : 'Sweden',
+    descrizione : 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    image : 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg'
+
+  }
+];
